fb6e1507bb58ac3af43097437bd4f97eb2a73b43
cjglo <chris.j.gallo3@gmail.com>
2021-03-31 22:31:22 -0400

    Reconfigured so used Traits for hashing, also removed debugiing printlns.

--- src/lib.rs
+++ src/lib.rs
@@ -6,48 +6,31 @@ struct HashTable<T> {
 
 impl<T> HashTable<T> {
 
+    // new method used for all generics
     pub fn new() -> HashTable<T> {
         
-        HashTable { // not a great way to init, but was running into issues with alloc and Copy Trait
+        HashTable { // not a great way to init, but this is just for practice so whatever
             arr: [ vec![], vec![], vec![], vec![], vec![], vec![], vec![], vec![], vec![], vec![], vec![], vec![], vec![] ],
         }
     }
 
-
-    pub fn is_in_hash(val: &T) -> bool {
-
-        unimplemented!();
-    }
-
 }
 
-// impl for Strings specifically
+// impl for Strings specifically (String and &str have Hash Trait which is below)
 impl HashTable<String> { // using String not &str because want ownership
     pub fn insert(&mut self, val: String) { 
 
-        let hash_num = HashTable::hash(&val);
-
-        println!("Hash for {0} is {1}", val, hash_num);
+        let hash_num = val.return_hash(13);  // Hard coded 13 cause this is just an excerise, normally would be variable
 
         self.arr[hash_num].push(val);
     }
 
-    fn hash(val: &str) -> usize {
-
-        let mut sum = 0;
-        
-        for c in val.chars() {
-            sum += c as usize;
-        }
-
-        sum % 13 // hard coded 13, (length of array), this is to attack my future self #TrustNoOne
-    }
 
     pub fn is_str_in_hash(&self, val: &str) -> bool {
 
         // set found to false, get the hash, find bucket length
         let mut found = false;
-        let hash_num = HashTable::hash(&val);
+        let hash_num = val.return_hash(13);
         let vec_length = self.arr[hash_num].len();
         // iter until found or at end of bucket
         for i in 0..vec_length {
@@ -60,11 +43,55 @@ impl HashTable<String> { // using String not &str because want ownership
 
 }
 
-trait hash {
-    // TODO: Plan is to impl a trait for each data type and use that in the generic HashTable
+// Trait used for hashing
+pub trait Hash {
+    
+    fn return_hash(&self, hash_size: usize) -> usize;
+}
+
+// impl used for my Hash that I included in this file
+impl Hash for String {
+
+    fn return_hash(&self, hash_size: usize) -> usize {
+        
+        let mut sum = 0;
+        
+        for c in self.chars() {
+            sum += c as usize;
+        }
+
+        sum % hash_size 
+    }
+
+}
+
+// impl used for hash look-up
+impl Hash for &str {
+
+    fn return_hash(&self, hash_size: usize) -> usize {
+        
+        let mut sum = 0;
+        
+        for c in self.chars() {
+            sum += c as usize;
+        }
+
+        sum % hash_size 
+    }
+
 }
 
 
+// Example impl of Hash for a char datatype
+impl Hash for char {
+
+    fn return_hash(&self, hash_size: usize) -> usize {
+
+        *self as usize * 35 % hash_size  // deref self as usize, multplied by 35 to spread out chars, then moduloed
+    }
+}
+
+// ! TESTS
 #[cfg(test)]
 mod tests 
 {
